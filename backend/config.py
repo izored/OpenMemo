7d7a4f225fff8a8d6f95e54bd9ce53ef9cacae47
izored <dev@izo.red>
2026-06-01 09:52:21 +0200

    release: v2.0.0

--- backend/config.py
+++ backend/config.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 class Settings(BaseSettings):
     APP_NAME: str = "OpenMemo"
-    VERSION: str = "1.8.5"
+    VERSION: str = "2.0.0"
     
     # Paths
     BASE_DIR: Path = Path(__file__).parent.parent
